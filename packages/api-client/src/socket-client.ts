618daa07308409a40a33781f1d001a511080e9c9
Subhadip-Paul2006 <subhadippaulff@gmail.com>
2026-08-10 08:28:40 +0530

    refactor(api-client): remove obsolete socket-client source file

--- packages/api-client/src/socket-client.ts
+++
@@ -1,8 +0,0 @@
-// Socket.io client wrapper placeholder for live queue updates
-export const initQueueSocket = (serverUrl: string) => {
-  return {
-    joinQueue: (doctorId: string, clinicId: string) => {},
-    leaveQueue: (doctorId: string, clinicId: string) => {},
-    onTokenUpdate: (callback: (data: any) => void) => {},
-  };
-};
